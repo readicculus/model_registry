71f91f32ccc295ffdd8707d68ea649235b820992
readicculus <yuval@yuvalboss.com>
2021-01-07 12:49:04 -0800

    test

---
+++ viame/2021_seal_pb_detectors/eo_polarbear_detector.sh
@@ -0,0 +1,13 @@
+ #!/bin/sh
+
+# Setup VIAME Paths (no need to run multiple times if you already ran it)
+
+export VIAME_INSTALL="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/../../../build/install"
+
+source ${VIAME_INSTALL}/setup_viame.sh
+
+# Run pipeline
+
+kwiver runner pipelines/eo_polarbear_detector.pipe \
+              -s optical_detector_input:video_filename=eo_list_tif.txt
+
