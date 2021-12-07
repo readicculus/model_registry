5c57be44286fe2b1c5d1d4e647faa72a2b0b56a8
readicculus <yuval@yuvalboss.com>
2021-12-07 12:25:05 -0800

    update tests

--- tests/test_commandline_pipes.py
+++ tests/test_commandline_pipes.py
@@ -21,7 +21,7 @@ class TestCommandLineDetectors(TestCaseBase):
     def test_eo_arctic_seal_yolo_pipe(self):
         images_fp = self.get_output_fp('output_images.txt')
         detections_fp = self.get_output_fp('output_detections.txt')
-        log_fp = self.get_output_fp('log.txt')
+        log_fp = self.get_kwiver_log_file()
         pipe_fp = os.path.abspath(os.path.join(PROJECT_ROOT,
                                                "packages/VIAME-JoBBS-Models/configs/pipelines/detector_eo_arctic_seal_yolo.pipe"))
         kwr = KwiverRunner(
@@ -46,7 +46,7 @@ class TestCommandLineDetectors(TestCaseBase):
     def test_eo_polar_bear_yolo_pipe(self):
         images_fp = self.get_output_fp('output_images.txt')
         detections_fp = self.get_output_fp('output_detections.txt')
-        log_fp = self.get_output_fp('log.txt')
+        log_fp = self.get_kwiver_log_file()
         pipe_fp = os.path.abspath(os.path.join(PROJECT_ROOT,
                                                "packages/VIAME-JoBBS-Models/configs/pipelines/detector_eo_polar_bear_yolo.pipe"))
         kwr = KwiverRunner(
@@ -71,7 +71,7 @@ class TestCommandLineDetectors(TestCaseBase):
     def test_ir_hotspot_pipe(self):
         images_fp = self.get_output_fp('output_images.txt')
         detections_fp = self.get_output_fp('output_detections.txt')
-        log_fp = self.get_output_fp('log.txt')
+        log_fp = self.get_kwiver_log_file()
         pipe_fp = os.path.abspath(os.path.join(PROJECT_ROOT,
                                                "packages/VIAME-JoBBS-Models/configs/pipelines/detector_ir_yolo.pipe"))
         kwr = KwiverRunner(
@@ -99,9 +99,11 @@ class TestCommandLineDetectors(TestCaseBase):
         images_fp_ir = self.get_output_fp('output_images_ir.txt')
         detections_fp_ir = self.get_output_fp('output_detections_ir.txt')
 
-        log_fp = self.get_output_fp('log.txt')
+        log_fp = self.get_kwiver_log_file()
         pipe_fp = os.path.abspath(os.path.join(PROJECT_ROOT,
                                                "packages/VIAME-JoBBS-Models/configs/pipelines/detector_pb_seal_yolo_ir_eo_region_trigger.pipe"))
+        transform_fp = os.path.abspath(os.path.join(PROJECT_ROOT,
+                                                    'packages/VIAME-JoBBS-Models/configs/pipelines/transformations/A90_RGB-IR_C_100mm_0deg_20190509-10_fl5-6.h5'))
         kwr = KwiverRunner(
             pipe_fp,
             {},
@@ -113,7 +115,8 @@ class TestCommandLineDetectors(TestCaseBase):
                 'thermal_writer_csv:frame_list_output': images_fp_ir,
                 'thermal_writer_csv:file_name': detections_fp_ir,
                 'optical_writer_csv:frame_list_output': images_fp_eo,
-                'optical_writer_csv:file_name': detections_fp_eo
+                'optical_writer_csv:file_name': detections_fp_eo,
+                'warp_ir_detections_to_eo:transformation_file': transform_fp
             }
         )
         with open(log_fp, 'w') as f:
--- tests/util.py
+++ tests/util.py
@@ -14,6 +14,9 @@ test_config = os.path.join(TEST_DIR, 'config.ini')
 config = configparser.ConfigParser()
 config.read(test_config)
 
+logging.basicConfig(format='%(asctime)s [%(levelname)s][%(name)s] - %(message)s', level=logging.DEBUG)
+global_logger = logging.getLogger(f'')
+
 def get_sealtk_dir():
     return config['TestConfig'].get('sealtk_setup')
 
@@ -25,7 +28,10 @@ class TestCaseBase(unittest.TestCase):
     temp_dir = os.path.join(TEST_DIR, 'tmp')
 
     def get_output_fp(self, fn):
-        return os.path.join(self.temp_dir, fn)
+        return os.path.join(self.temp_dir, self._testMethodName + '_' + fn)
+
+    def get_kwiver_log_file(self):
+        return os.path.join(self.temp_dir, self._testMethodName + '.log')
 
     def assertIsFile(self, path):
         if not pl.Path(path).resolve().is_file():
@@ -35,35 +41,27 @@ class TestCaseBase(unittest.TestCase):
         if not pl.Path(path).resolve().is_dir():
             raise AssertionError("Directory does not exist: %s" % str(path))
 
-    def _setup(self):
-        self.log = logging.getLogger(self.id())
-        self.print('Test Started', logging.DEBUG)
 
-
-    # print to test log
     def print(self, message, loglevel=logging.INFO):
-        if not self.log:
-            self._setup()
-
         self.log.log(level=loglevel, msg=message)
 
     def setUp(self) -> None:
-        self._setup()
-        if os.path.isdir(self.temp_dir):
-            shutil.rmtree(self.temp_dir)
+        self.log = logging.getLogger(self.id())
+        # if os.path.isdir(self.temp_dir):
+        #     shutil.rmtree(self.temp_dir)
         os.makedirs(self.temp_dir, exist_ok=True)
 
     def tearDown(self) -> None:
         try:
             if self._outcome.success:
-                self.print('Test Succeeded', logging.DEBUG)
+                self.print('Test Succeeded', logging.INFO)
+                # if os.path.isdir(self.temp_dir):
+                #     shutil.rmtree(self.temp_dir)
             else:
                 self.print('Test Failed', logging.ERROR)
         except:
             pass
 
-        if os.path.isdir(self.temp_dir):
-            shutil.rmtree(self.temp_dir)
 
     # @classmethod
     # def setUpClass(cls) -> None:
@@ -72,6 +70,7 @@ class TestCaseBase(unittest.TestCase):
     #
     #     os.makedirs(cls.temp_dir, exist_ok=True)
 
-    # @classmethod
-    # def tearDownClass(cls) -> None:
-    #     shutil.rmtree(cls.temp_dir)
+    @classmethod
+    def tearDownClass(cls) -> None:
+        if os.path.isdir(cls.temp_dir):
+            shutil.rmtree(cls.temp_dir)
